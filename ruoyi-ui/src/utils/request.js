c75ab0b8d778aa69ac52484d1bedc5e9778ffe51
ssz <sszvip@qq.com>
2025-05-13 16:41:04 +0800

    server info

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -82,7 +82,7 @@ service.interceptors.response.use(res => {
       return res.data
     }
     if (code === 401) {
-      debugger
+      // debugger
       if (!isRelogin.show) {
         isRelogin.show = true;
         MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
