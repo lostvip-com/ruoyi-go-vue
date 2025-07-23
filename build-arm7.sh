d5028974d693c73ca3132a07b65d056151ada8a3
sszvip@qq.com <sszvip@qq.com>
2025-07-23 14:20:03 +0800

    ddd

--- build-arm7.sh
+++ build-arm7.sh
@@ -1,4 +1,4 @@
-docker build -t cgo-arm-softfp .
+docker build -t cgo-arm .
 
 #方式1 纯go CGO_ENABLED=0
 #docker run --rm -v "$PWD":/workspace \
@@ -9,9 +9,9 @@ docker build -t cgo-arm-softfp .
 #  golang:1.24-bullseye \
 #  go build -ldflags '-w -s' -o app-arm7 ./cmd
 
-#方式2 CGO_ENABLED=1
+#方式2 CGO_ENABLED=1 软浮点
 docker run --rm -v "$PWD":/workspace \
-  cgo-arm-softfp \
+  cgo-arm \
   go build -ldflags '-w -s' -o app-arm7 ./cmd
 
 
