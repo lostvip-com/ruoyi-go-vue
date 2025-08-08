03674cc3341df2b78b640954ae1dd7a72ba4cf5c
sszvip@qq.com <sszvip@qq.com>
2025-08-08 11:50:53 +0800

    reflect获取值

--- test/reflect_test.go
+++ test/reflect_test.go
@@ -2,7 +2,10 @@ package test
 
 import (
 	"fmt"
+	"github.com/lostvip-com/lv_framework/lv_log"
+	"github.com/lostvip-com/lv_framework/utils/lv_json"
 	"github.com/lostvip-com/lv_framework/utils/lv_reflect"
+	"github.com/spf13/cast"
 	"testing"
 )
 
@@ -16,7 +19,15 @@ func TestReflect(t *testing.T) {
 			Street: "长安街",
 		},
 	}
-
+	user2 := &User{
+		ID:   2,
+		Name: "李四",
+		Age:  25,
+		Address: Address{
+			City:   "北京",
+			Street: "长安街",
+		},
+	}
 	// 获取简单字段
 	if val, ok := lv_reflect.GetFieldValue(user, "Name"); ok {
 		fmt.Println("Name:", val) // 输出: Name: 张三
@@ -36,6 +47,22 @@ func TestReflect(t *testing.T) {
 	if val, ok := lv_reflect.GetFieldValueSimple(user, "Name"); ok {
 		fmt.Println("Name (simple):", val) // 输出: Name (simple): 张三
 	}
+	list := make([]User, 0)
+	list = append(list, *user)
+	list = append(list, *user2)
+	mp := make(map[string]User)
+	mapKey := "ID"
+	for i := range list {
+		it := list[i]
+		value, ok := lv_reflect.GetFieldValueSimple(it, mapKey)
+		if ok {
+			mp[cast.ToString(value)] = it
+		} else {
+			lv_log.Warn("mapKey not found", mapKey)
+		}
+	}
+	fmt.Println("Name (simple):", lv_json.ToJsonStr(list)) // 输出: Name (simple): 张三
+	fmt.Println("Name (simple):", lv_json.ToJsonStr(mp))   // 输出: Name (simple): 张三
 }
 
 // 示例结构体
